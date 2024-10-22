099c7c99bcd9e04d57a200d608799c1ebc6e4bd2
Kevin Alan Soto Ramirez <95001162+KevinA017@users.noreply.github.com>
2024-10-22 10:46:01 -0700

    Shop_1

--- src/App.js
+++ src/App.js
@@ -1,9 +1,14 @@
 import logo from './logo.svg';
 import './App.css';
 import NavBar from './components/NavBar';
+import Shop_1 from './components/Shop_1';
+
 function App() {
   return (
+    <>
     <NavBar />
+    <Shop_1 />
+    </>
   );
 }
 
--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -69,7 +69,7 @@ const NavBar = () => {
                   className="rounded-md bg-gray-900 px-3 py-2 text-sm font-medium text-white"
                   aria-current="page"
                 >
-                  Dashboard
+                  Inicio
                 </a>
                 <a
                   href="#"
@@ -87,7 +87,7 @@ const NavBar = () => {
                   href="#"
                   className="rounded-md px-3 py-2 text-sm font-medium text-gray-300 hover:bg-gray-700 hover:text-white"
                 >
-                  Calendar
+                  Catagorias
                 </a>
               </div>
             </div>
