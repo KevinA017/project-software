b074ecd0b401f044ec8202f9bf938e516cd8c5fa
Marco Rios <117676726+RiosMarko@users.noreply.github.com>
2024-10-22 18:51:22 -0700

    Add files via upload

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -25,6 +25,11 @@ const NavBar = () => {
     navigate('/login');
   };
 
+  const handleSignUpClick = () => {
+    navigate('/signup'); 
+  };
+
+
   const handleDashboardClick = () => {
     navigate('/'); // Esto navegará a la ruta raíz donde está Shop_1
   };
@@ -144,7 +149,8 @@ const NavBar = () => {
               <div className="flex items-center space-x-4">
               <button
                 className="rounded-md px-3 py-2 text-sm font-medium text-gray-300 hover:bg-gray-700 hover:text-white"
-                onClick={() => navigate('/signup')}
+                onClick={handleSignUpClick}
+                
               >
                 <FontAwesomeIcon icon={faUserPlus} className="mr-2" />
                 Sign Up
