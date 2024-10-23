56627cbf91bcf9d18ee1079d70e70b85bacb6338
Marco Rios <117676726+RiosMarko@users.noreply.github.com>
2024-10-22 18:51:50 -0700

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -3,8 +3,11 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import './App.css';
 import NavBar from './components/NavBar';
 import Login from './components/Login';
+import SignUp from './components/SignUp';
 import Shop_1 from './components/Shop_1';
 import Footer from './components/Footer'
+
+
 function App() {
   return (
     <Router>
@@ -13,8 +16,9 @@ function App() {
         <Routes>
           <Route path="/" element={<Shop_1 />} />
           <Route path="/login" element={<Login />} />
+          <Route path="/signup" element={<SignUp />} />
         </Routes>
-        <Footer></Footer>
+        <Footer />
       </div>
     </Router>
   );
