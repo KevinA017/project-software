de15bfd7e2fbf494752b1e8797bca5d2d4b1027b
Kevin <kevinalansoto@gmail.com>
2024-11-25 22:44:49 -0700

    Git pages

--- src/App.js
+++ src/App.js
@@ -1,31 +1,36 @@
-import logo from './logo.svg';
 import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
 import { AuthProvider, useAuth } from './AuthContext';
-import './App.css';
 import NavBar from './components/NavBar';
 import Login from './components/Login';
 import SignUp from './components/SignUp';
 import Shop_1 from './components/Shop_1';
-import Footer from './components/Footer'
+import Footer from './components/Footer';
 import ExpenseForm from './components/ExpenseForm';
 import ExpenseCalendar from './components/ExpenseCalendar';
-import Dashboard from './components/Dashboard'
-import SavingsPlan from './components/SavingsPlan'
+import Dashboard from './components/Dashboard';
+import SavingsPlan from './components/SavingsPlan';
+import './App.css';
+
+// Ajustar el `basename` para despliegue en GitHub Pages
+const basename = process.env.PUBLIC_URL || '';
 
 function App() {
   return (
     <AuthProvider>
-      <Router>
-        <div>
-          <NavBar/>
+      <Router basename={basename}>
+        <div className="App">
+          <NavBar />
           <Routes>
+            {/* Rutas públicas */}
             <Route path="/" element={<Shop_1 />} />
             <Route path="/login" element={<Login />} />
             <Route path="/signup" element={<SignUp />} />
-            <Route path='/expense' element={<ExpenseForm />}/>
-            <Route path='/calendar' element={<ExpenseCalendar/>}/>
-            <Route path='/dashboard' element={<Dashboard/>}/>
-            <Route path='/savingsPlan' element={<SavingsPlan/>}/>
+
+            {/* Rutas protegidas */}
+            <Route path="/expense" element={<PrivateRoute><ExpenseForm /></PrivateRoute>} />
+            <Route path="/calendar" element={<PrivateRoute><ExpenseCalendar /></PrivateRoute>} />
+            <Route path="/dashboard" element={<PrivateRoute><Dashboard /></PrivateRoute>} />
+            <Route path="/savingsPlan" element={<PrivateRoute><SavingsPlan /></PrivateRoute>} />
           </Routes>
           <Footer />
         </div>
@@ -33,15 +38,13 @@ function App() {
     </AuthProvider>
   );
 }
+
+// Rutas protegidas: Solo usuarios autenticados pueden acceder
 const PrivateRoute = ({ children }) => {
   const { user } = useAuth();
 
-  // Si no hay usuario, redirige a la página de inicio de sesión
-  if (!user) {
-      return <Navigate to="/login" replace />;
-  }
-
-  return children;  // Si hay un usuario, muestra el contenido protegido
+  // Si no hay usuario, redirige a login
+  return user ? children : <Navigate to="/login" replace />;
 };
 
-export default App;
+export default App;
